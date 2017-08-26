49edb6b179459bec49c5a43c780f5900588f5273
Marwan <marouane.bigoook@gmail.com>
2017-08-26 22:03:31 +0100

    Simple Data Type Char/String/BoolTypes
    Simple Data Type Char/String/BoolTypes Part1 الأنواع البسيطة حرفي/نصي/المنطقي

---
+++ my_Simple_Data_Typ_ Char_String_Boo_Types/my_Simple_Data_Typ_ Char_String_Boo_Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_Simple_Data_Typ__Char_String_Boo_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+             
+            int my_max_char = char.MaxValue;
+            int my_min_char = char.MinValue;
+
+            char my_char = 'a';
+
+            string my_string = "my first string";
+
+           char my_convert_tochar =  Convert.ToChar(97);
+
+            Console.WriteLine("\n my_max_char= {0}\n my_min_char= {1}\n my_char= {2}\n my_string= {3}\n my_convert_tochar= {4}", my_max_char, my_min_char, my_char, my_string, my_convert_tochar);
+            Console.ReadKey();
+        }
+    }
+}
