33f306351b6b92fac82f0dd59c49e22fbb62ee1b
Marwan <marouane.bigoook@gmail.com>
2017-09-29 15:29:32 +0100

    my_Expressions
     في هذا الدرس سنتعلم طريقة التعامل مع التعابير وكيفية الحصول على التعبير وماهو الفرق بين الــExpression والـــOperand  والـــOperator.....مشاهدة ممتعة ولاتنسى المشاركة والأشتراك في القناة ليصلك المزيد من الفيديوات المتميزة

---
+++ my_Expressions/my_Expressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expressions التعابير
+
+            float my_Operand1 = 10.5f; //Operand 1
+            float my_operand2 = 10.7f; //Operand 2 
+
+              float my_temp = my_Operand1 + my_operand2; //Expressions 
+
+            // basic Operators (+ - * / )
+            // bitwise Operators
+            // boolean Operators true false
+
+            // 1- unary Operators
+            // 2- Binary Operators
+            // 3- 3 Operators
+        }
+
+
+
+    }   
+}
+
